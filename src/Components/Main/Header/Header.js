a470fe49f6e6defd186edf74ea15c52721291159
AsaduzzamanAshik1610 <mdashik1610@gmail.com>
2022-11-07 21:46:22 +0600

    first commit

---
+++ src/Components/Main/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+         <div className="navbar bg-primary text-primary-content">
+         <h4 className='text-white'>hospitality</h4>
+         <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+       </div>
+    </div>
+);
+};
+
+export default Header;
