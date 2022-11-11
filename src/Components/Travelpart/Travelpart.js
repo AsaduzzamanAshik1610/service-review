303b17a10b9d0f3ee0ba5bb81e8a28af3ad4ca9e
AsaduzzamanAshik1610 <mdashik1610@gmail.com>
2022-11-11 23:31:51 +0600

    added the beach side

--- src/Components/Travelpart/Travelpart.js
+++ src/Components/Travelpart/Travelpart.js
@@ -4,11 +4,12 @@ import pic from '../../Components/assest/images/cox.5.jpg'
 const Travelpart = () => {
     return (
         <div>
-            <div className="card card-compact w-96 bg-base-100 shadow-xl">
+            <div className=" mt-5 card card-compact w-96 bg-base-100 mx-auto shadow-xl">
                 <figure><img src={pic} alt="Shoes" /></figure>
                 <div className="card-body">
-                    <h2 className="card-title">Shoes!</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <h2 className="card-title">Coxbazer beach side</h2>
+                    <p>onek popular resort saradeser manush ekhane ase moja kore. tachara amader desher onek boro beach deshe bidesh theke onek porjotok ase </p>
+                    <h2 className='text-orange-600'>cost: 2000tk</h2>
                     <div className="card-actions justify-end">
                         <Link to='/travel'>
                           <button className="btn btn-primary">travel</button>
